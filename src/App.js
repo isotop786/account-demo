0242ed73f49f2f41f7c530a977ab9c2782ba56d3
isotop786 <marufislam77@gmail.com>
2022-12-05 17:57:23 +0600

    style update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import TableComponent from "./Components/TableComponent";
 
 const App = () => {
   return (
-    <Row style={{padding:"1rem", paddingTop:"3rem",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <Row style={styles.container}>
         <TableComponent/>
     </Row>
   )
+}
+
+const styles = {
+  container: {
+    padding: "1rem",
+    paddingTop: "3rem",
+    display: "flex",
+    justifyContent: "center", alignItems: "center"
+ }
 }
 
 export default App;
